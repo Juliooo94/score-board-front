7e27f1a0d6d631bb30aaea4caabf78b3b4192f12
Julie Agopian <ext.malt-community-sa.jagopian@systra.com>
2025-05-06 17:14:45 +0200

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+
+const message = ref('Loading...')
+
+
+onMounted(async () => {
+  try {
+    const res = await fetch('http://localhost:8000')
+    const data = await res.json()
+    message.value = data.message
+  } catch (error) {
+    message.value = 'Failed to fetch message'
+  }
+})
+</script>
+
+<template>
+  <div class="card">
+    <p>
+      {{ message }}
+    </p>
+  </div>
+</template>
+
+<style scoped>
+</style>
